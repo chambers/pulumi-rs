c46f351304e5127f52cd54241236b6c5229c2365
Kieran Chambers <kieranchambers@gmail.com>
2023-09-20 20:33:31 +0100

    add all protos and compile all the things

--- build.rs
+++ build.rs
@@ -1,9 +1,39 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+use eyre::Result;
+fn main() -> Result<(), eyre::Report> {
     tonic_build::configure()
+        .build_client(true)
         .build_server(false)
+        .compile_well_known_types(false)
+        .out_dir("src/proto/pulumi/codegen")
+        .compile(
+            &[
+                "proto/pulumi/codegen/hcl.proto",
+                "proto/pulumi/codegen/loader.proto",
+                "proto/pulumi/codegen/mapper.proto",
+            ],
+            &["proto"],
+        )?;
+
+    tonic_build::configure()
+        .build_client(true)
+        .build_server(false)
+        .compile_well_known_types(false)
         .out_dir("src/proto/pulumi")
-        //arg1=protos to compile, arg2=root folder of protos
-        .compile(&["proto/pulumi/errors.proto"], &["proto"])?;
+        .compile(
+            &[
+                "proto/pulumi/resource.proto",
+                "proto/pulumi/alias.proto",
+                "proto/pulumi/source.proto",
+                "proto/pulumi/alias.proto",
+                "proto/pulumi/plugin.proto",
+                "proto/pulumi/errors.proto",
+                "proto/pulumi/analyzer.proto",
+                "proto/pulumi/language.proto",
+                "proto/pulumi/provider.proto",
+                "proto/pulumi/converter.proto",
+            ],
+            &["proto"],
+        )?;
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
-//use std::path::PathBuf;
-
 #[path = "proto/"]
 pub mod proto {
     #[path = "proto/pulumi"]
     pub mod pulumi {
         include!("proto/pulumi/pulumirpc.rs");
+        #[path = "proto/pulumi/codegen"]
+        pub mod codegen {
+            include!("proto/pulumi/pulumirpc.codegen.rs");
+        }
     }
 }
-fn main() {
-    let error_cause = proto::pulumi::ErrorCause {
-        message: String::from("This is an error message"),
-        stack_trace: String::from("This is a stack trace"),
-    };
 
-    let message = error_cause.message;
-    println!("Error message: {}", message);
+fn main() {
+    // let error_cause = proto::pulumi::ErrorCause {
+    //     message: String::from("This is an error message"),
+    //     stack_trace: String::from("This is a stack trace"),
+    // };
+
+    // let message = error_cause.message;
+    // println!("Error message: {}", message);
 }
